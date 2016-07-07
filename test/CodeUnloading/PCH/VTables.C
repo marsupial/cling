f1c948c660937e2fc83c453fa384b66d11667c59
Frederich Munch <marsupial@users.noreply.github.com>
2016-07-07 01:51:15 -0400

    Add support to use 'pure' llvm/clang libraries that only change their behavior when cling is a client of them.  Both clang and cling to pass their test suites,  allowing one installation for both cling and llvm/clang.
    Enabled via cmake CLING_CLANG_RUNTIME_PATCH flag.

--- test/CodeUnloading/PCH/VTables.C
+++ test/CodeUnloading/PCH/VTables.C
@@ -3,8 +3,11 @@
 // RUN: mkdir -p %T/Rel/Path
 // RUN: clang -x c++-header -fexceptions -fcxx-exceptions -std=c++14 -pthread %S/Inputs/CompGen.h -o CompGen.h.pch
 // RUN: clang -x c++-header -fexceptions -fcxx-exceptions -std=c++14 -pthread %S/Inputs/CompGen.h -o %T/Rel/Path/Relative.pch
-// RUN: cat %s | %cling -I%p -Xclang -include-pch -Xclang CompGen.h.pch  2>&1 | FileCheck %s
-// RUN: cat %s | %cling -I%p -I%T/Rel/Path -include-pch Relative.pch 2>&1 | FileCheck %s
+// RUN: cat %s | %cling -I%p -Xclang -include-pch -Xclang CompGen.h.pch  2>&1 | FileCheck --check-prefixes CHECK,WARN %s
+// RUN: cat %s | %cling -I%p -I%T/Rel/Path -include-pch Relative.pch 2>&1 | FileCheck --check-prefixes CHECK,WARN %s
+// RUN: cat %s | %cling -Wno-invalid-pch -I%p -Xclang -include-pch -Xclang CompGen.h.pch  2>&1 | FileCheck %s
+
+// WARN: warning: please rebuild precompiled header '{{.*}}' with with cling [-Winvalid-pch]
 
 //.storeState "a"
 .x TriggerCompGen.h
