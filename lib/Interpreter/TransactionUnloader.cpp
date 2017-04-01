512ae3a7874cc60899e12ab2d02b4c58fdf9ccd2
Frederich Munch <marsupial@users.noreply.github.com>
2017-03-31 22:16:04 -0400

    Track decls that were emitted deferred in Transaction and mark un-emitted on undo.

--- lib/Interpreter/TransactionUnloader.cpp
+++ lib/Interpreter/TransactionUnloader.cpp
@@ -117,6 +117,8 @@ namespace cling {
       // }
 
       Successful = unloadModule(T->getModule()) && Successful;
+
+      m_CodeGen->restoreEmittedDeferred(T->getDeferred());
     }
 
     // Clean up the pending instantiations
