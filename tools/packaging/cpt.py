9e27805ef637960a6e809cbefef19f0f968461de
Frederich Munch <marsupial@users.noreply.github.com>
2017-06-18 22:45:56 -0400

    ***** Run Test *****

--- tools/packaging/cpt.py
+++ tools/packaging/cpt.py
@@ -618,7 +618,7 @@ def runSingleTest(test, Idx = 2, Recurse = True):
 def test_cling():
     box_draw("Run Cling test suite")
     # Run single tests on CI with this
-    runSingleTest('Prompt/ValuePrinter/ExplicitInclude.C')
+    runSingleTest('Lookup/template.C')
     build = Build('check-cling')
 
 def tarball():
