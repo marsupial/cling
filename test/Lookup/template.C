3b4d1eb274396173bd41002397095b2512919f3d
Frederich Munch <marsupial@users.noreply.github.com>
2017-06-18 22:45:16 -0400

    Fix test for gcc-6.

--- test/Lookup/template.C
+++ test/Lookup/template.C
@@ -35,24 +35,24 @@ cling::LookupHelper::DiagSetting diags = cling::LookupHelper::WithDiagnostics;
 
 const clang::ClassTemplateDecl* tmplt_out = lookup.findClassTemplate("TmpltOutside", diags);
 
-printf("tmplt_out: 0x%lx\n", (unsigned long) tmplt_out);
-//CHECK: tmplt_out: 0x{{[1-9a-f][0-9a-f]*$}}
+printf("tmplt_out: %p\n", tmplt_out);
+//CHECK: tmplt_out: {{[x0-9a-f]+}}
 tmplt_out->getQualifiedNameAsString().c_str()
 //CHECK-NEXT: ({{[^)]+}}) "TmpltOutside"
 
 
 const clang::ClassTemplateDecl* tmplt_inside = lookup.findClassTemplate("OuterClass::TmpltInside", diags);
 
-printf("tmplt_inside: 0x%lx\n", (unsigned long) tmplt_out);
-//CHECK: tmplt_inside: 0x{{[1-9a-f][0-9a-f]*$}}
+printf("tmplt_inside: %p\n", tmplt_out);
+//CHECK: tmplt_inside: {{[x0-9a-f]+}}
 tmplt_inside->getQualifiedNameAsString().c_str()
 //CHECK-NEXT: ({{[^)]+}}) "OuterClass::TmpltInside"
 
 
 const clang::ClassTemplateDecl* tmplt_vec = lookup.findClassTemplate("std::vector", diags);
 
-printf("tmplt_vec: 0x%lx\n", (unsigned long) tmplt_vec);
-//CHECK: tmplt_vec: 0x{{[1-9a-f][0-9a-f]*$}}
+printf("tmplt_vec: %p\n", tmplt_vec);
+//CHECK: tmplt_vec: {{[x0-9a-f]+}}
 tmplt_vec->getQualifiedNameAsString().c_str()
 //CHECK-NEXT: ({{[^)]+}}) "std::{{(__1::)?}}vector"
 
