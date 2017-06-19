d2faa672eafcd5d4ce15806710a2d9c5d1353718
Frederich Munch <marsupial@users.noreply.github.com>
2017-06-19 12:49:39 -0400

    TextInput: Delay static initialization until needed.

--- lib/UserInterface/textinput/History.h
+++ lib/UserInterface/textinput/History.h
@@ -38,8 +38,10 @@ namespace textinput {
 
     // Indices are reverse! I.e. 0 is newest!
     const std::string& GetLine(size_t Idx) const {
-      static const std::string sEmpty;
-      if (Idx == (size_t)-1) return sEmpty;
+      if (Idx == (size_t)-1)  {
+        static const std::string sEmpty;
+        return sEmpty;
+      }
       return fEntries[fEntries.size() - 1 - Idx];
     }
     size_t GetSize() const { return fEntries.size(); }
