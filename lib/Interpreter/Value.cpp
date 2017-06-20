6c4a099e61427c75b77a51f5bcf1e88a471d3fa6
Frederich Munch <marsupial@users.noreply.github.com>
2017-06-20 12:31:05 -0400

    Fixes for CERN/master.
    Currently the bit is not being set in ValueExtractionSynthesizer which would mean no destructor would ever run!

--- lib/Interpreter/Value.cpp
+++ lib/Interpreter/Value.cpp
@@ -137,6 +137,10 @@ namespace {
     AllocatedValue(char Info) {
       m_Count = 0;
       m_Bytes[FlagsByte] = Info;
+#if 1
+      // FIXME: Set this properly in ValueExtractionSynthesizer::Transform.
+      m_Bytes[FlagsByte] |= kConstructorRan;
+#endif
       UpdateRefCount(1);
     }
 
